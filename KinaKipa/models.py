15597fb1c0d2894df8bd1048245e07d56419f12e
AlexanderdeI <semionchik.a.g@gmail.com>
2017-12-02 15:58:29 +0300

    Connected MySQL db, created simple model

--- KinaKipa/models.py
+++ KinaKipa/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.db.models import CharField
 
-# Create your models here.
+
+class Article(models.Model):
+    title = CharField(max_length=200, verbose_name='article_title')
