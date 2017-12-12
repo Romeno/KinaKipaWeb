b662fad4464b0e45dc32926620ce6bbfbe3973bc
Roman Troshkin <RomenoEx@gmail.com>
2017-12-12 19:48:44 +0300

    added flatpages to dumpdata

--- dumpdata.bat
+++ dumpdata.bat
@@ -1,3 +1,3 @@
 @echo off
-python ./manage.py dumpdata KinaKipa --indent 4  --output KinaKipa\fixtures\initial_data.json
+python ./manage.py dumpdata KinaKipa flatpages --indent 4  --output KinaKipa\fixtures\initial_data1.json
 PAUSE
