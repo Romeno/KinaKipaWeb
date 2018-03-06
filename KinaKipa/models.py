7ed72fc9cec6346681e71786898a863801a619a8
Artyom Giletich <giletichart@gmail.com>
2018-03-06 22:50:35 +0300

    begin to work with slider

--- KinaKipa/models.py
+++ KinaKipa/models.py
@@ -194,6 +194,8 @@ class HeroSlide(HeroSlideBase):
     content = HTMLField(verbose_name=_('Hero slide text'), help_text=_('Hero slide text'))
     button_caption = CharField(max_length=200, verbose_name=_('Button caption'), help_text=_("Button caption"))
 
+    def __str__(self):
+        return f'[{self.button_caption}]: "{self.content}"'
 
 class MovieHeroSlide(HeroSlideBase):
     movie = models.ForeignKey(Film, verbose_name=_('Hero movie'))
